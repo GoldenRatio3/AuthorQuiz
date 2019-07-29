cf6b86fd221e97c8fc5316e96c5d871c5d46ac42
Brad Winter <brad@brads-mbp.mynet>
2019-07-29 21:48:04 +0100

    Added author details

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,12 +1,53 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./bootstrap.min.css";
 
-class AuthorQuiz extends React.Component {
-  render() {
-    return <h1>Author Quiz</h1>;
-  }
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorImage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Continue() {
+  return <div />;
+}
+
+function AuthorQuiz({ turnData }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
