f9da7b6dc3b1480b69043992e49de71da4e9ae3e
Brad Winter <bradleyswinter@hotmail.co.uk>
2019-10-30 17:53:54 +0000

    Updates

---
+++ precache-manifest.bb00bbb786bbf20ebd16e49b8e85ca69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6960cc5ea6f7a0c102251e86ee7881",
+    "url": "/AuthorQuiz/index.html"
+  },
+  {
+    "revision": "60e33b0a35e96acf7248",
+    "url": "/AuthorQuiz/static/css/main.e9aceb1c.chunk.css"
+  },
+  {
+    "revision": "040cd92ee875be99556d",
+    "url": "/AuthorQuiz/static/js/2.f98b8356.chunk.js"
+  },
+  {
+    "revision": "60e33b0a35e96acf7248",
+    "url": "/AuthorQuiz/static/js/main.ef5c06a3.chunk.js"
+  },
+  {
+    "revision": "ba0040e62579613d99c0",
+    "url": "/AuthorQuiz/static/js/runtime~main.500bd57c.js"
+  }
+]);
